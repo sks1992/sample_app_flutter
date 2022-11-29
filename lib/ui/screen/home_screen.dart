6d11c62ff2bf9dab4a7903db7c83dfb41416e1c1
Sandeep <sandeepsk15892>
2022-11-29 18:00:23 +0530

    sample app

--- lib/ui/screen/home_screen.dart
+++ lib/ui/screen/home_screen.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../core/controller/auth_controller.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+   HomeScreen({Key? key}) : super(key: key);
+  final _authController = Get.find<AuthController>();
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Home"),
+        actions: [IconButton(onPressed: ()async {
+          await _authController.logout();
+        }, icon:  const Icon(Icons.logout))],
       ),
       body: const Center(
         child: Text("Home"),
