85e7930b02e413f0e58b526ed7b27da8c751e0f0
Sandeep <sandeepsk15892>
2022-11-29 17:54:45 +0530

    sample app

--- lib/core/controller/auth_controller.dart
+++ lib/core/controller/auth_controller.dart
@@ -3,7 +3,6 @@ import 'package:get/get.dart';
 import '../../app_routes.dart';
 import '../bindings/local_storage_binidngs.dart';
 import '../constants/constants.dart';
-import '../service/shared_service.dart';
 
 class AuthController extends GetxController {
   var loggedIn = false.obs;
@@ -31,13 +30,7 @@ class AuthController extends GetxController {
   }
 
   Future logout() async {
-    final _sharedService = Get.find<SharedService>();
     LocalStorage.sharedInstance.deleteValue(Constants.userAuthToken);
-    try {
-      //await revokeAuthAndRefreshTokens(_token);
-    } catch (e) {
-      //ignore}
-    }
     loggedIn.value = false;
   }
 }
